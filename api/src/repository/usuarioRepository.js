cdb323e3f16dfdb933b8865a2db29963d8de5797
Joao-Gustavo-C <joaogustavocarmomoraes@gmail.com>
2022-06-07 18:21:52 -0300

    sla

--- api/src/repository/usuarioRepository.js
+++ api/src/repository/usuarioRepository.js
@@ -3,7 +3,7 @@ import {con} from '../controller/connection.js'
 
 export async function vilao(vilao) {
     const comando = 
-    `SELECT * from tb_vilao`;
+    `SELECT`;
 
     const [a] = await con.query (comando, [vilao])
     return a[0];
